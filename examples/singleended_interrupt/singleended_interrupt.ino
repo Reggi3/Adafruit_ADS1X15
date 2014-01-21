dbfd51addbf07b1d20c0d6ec3838b77966edf0af
Alon Levy <alevy@redhat.com>
2014-01-21 18:35:16 +0200

    working singleended_interrupt with two channels, ascii

--- examples/singleended_interrupt/singleended_interrupt.ino
+++ examples/singleended_interrupt/singleended_interrupt.ino
@@ -7,40 +7,51 @@ Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
 // RFDuino pin connected to ADS ALERT/RDY pin
 #define GPIO_RDY 2
 
+#define NUM_CHANNELS 2
+
+#if NUM_CHANNELS == 1
+const uint8_t channels[1] = {0};
+#elif NUM_CHANNELS == 2
+const uint8_t channels[2] = {0, 1};
+#endif
+
 static volatile uint16_t total_isr;
 static volatile uint16_t cur_isr;
 static volatile uint16_t cur_val;
+static uint8_t channel_num;
 
 int isr(uint32_t ulPin)
 {
   total_isr++;
   cur_isr++;
   cur_val = ads.getLastConversionResults();
+  channel_num = (channel_num + 1) % NUM_CHANNELS;
+  ads.setContinuousMode(channels[channel_num]);
   return 0;
 }
 
-void setup(void) 
+void setup(void)
 {
   Serial.begin(115200);
   total_isr = cur_isr = 0;
+  channel_num = 0;
   
   pinMode(GPIO_RDY, INPUT);
   RFduino_pinWakeCallback(GPIO_RDY, LOW, isr);
-  ads.beginContinuous(0);
-  while (1) {
-    Serial.write('b');
-  }
-  while(!Serial.available());
-  while(Serial.available())
-    Serial.read();
+  ads.beginContinuousMode(0);
+  
+  //while(!Serial.available());
+  //while(Serial.available()) {
+  //  Serial.read();
+  //}
 }
 
 static void write_int(uint16_t val)
 {
     uint8_t c;
-    c = (val & 0xff00) >> 16;
-    Serial.write(c);
     c = val & 0xff;
+    Serial.write(c);
+    c = (val & 0xff00) >> 8;
     Serial.write(c);
 }
 
@@ -48,6 +59,7 @@ void loop(void)
 {
   if (cur_isr > 0) {
     cur_isr = 0;
-    write_int(cur_val);
+    //write_int(cur_val);
+    Serial.println(cur_val);
   }
 }
